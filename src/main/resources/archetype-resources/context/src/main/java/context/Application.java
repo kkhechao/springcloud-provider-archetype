242e96d238a95cc619f4a5b1b78c77ef55c3fd34
chao <512271472@qq.com>
2019-01-11 17:51:29 +0800

    222

---
+++ src/main/resources/archetype-resources/context/src/main/java/context/Application.java
@@ -0,0 +1,27 @@
+#set($symbol_pound='#')
+#set($symbol_dollar='$')
+#set($symbol_escape='\' )
+package ${package}.context;
+
+import com.jovezhao.nest.starter.EnableNest;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableNest
+@MapperScan("${package}.context.appservice.impl.domain.repository")
+@ComponentScans({
+        @ComponentScan("${package}.context"),
+        @ComponentScan("com.zqkh.common.configuration")
+})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/resources/archetype-resources/context/src/main/java/context/appservice/impl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.context.appservice.impl;
+
+import com.jovezhao.nest.starter.AppService;
+import ${package}.context.appservice.inter.UserService;
+
+@AppService
+public class UserServiceImpl implements UserService{
+
+}
